41ace906faafc877ca5e512097c2c17916080d87
Shushan907 <shushan907@gmail.com>
2020-06-05 01:50:09 +0400

    added default user location, corrected code (more understood code)

--- js/script.js
+++ js/script.js
@@ -1,19 +1,44 @@
-//------------------GetWeatherInfo-------------------------------------
+//------------------Variabels-------------------------------------
 
-const API_URL = 'https://api.openweathermap.org/data/2.5/weather?q=';
+const API_URL = 'https://api.openweathermap.org/data/2.5/';
+const FLAG_URL = 'https://countryflags.io/';
+const TEMP_URL = 'http://openweathermap.org/img/w/';
 const API_KEY = '40a3ab422b6c7446253471c3714edfb8';
-const flagCountry = document.getElementById('CountryImg'); 
-const iconTemp = document.getElementById('tempIcon');
+const iconTemp = document.querySelector('.tempIcon');
+const flag = document.querySelector('.flag');
 
 //-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 
-//----------------------BOM navigator-----------------------------------
+//------------------Helping functions-----------------------------------
+
+function setQS(selector, value) {
+  document.querySelector(selector).innerText = value; 
+}
+
+//-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
+function setCityWeather(info) {
+    setQS('.cityResult', info.name);
+    setQS('.countryResult', info.sys.country);
+    setQS('.temperatureResult', `${Math.round(info.main.temp)}°C`);
+    flag.src = `${FLAG_URL}${info.sys.country}/flat/64.png`;
+    iconTemp.src = `${TEMP_URL}${info.weather[0].icon}.png`;
+}
+
+//-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
+//----------------------Navigator---------------------------------------
 
 (function () {
     window.navigator.geolocation.getCurrentPosition((position) => {
         const { latitude, longitude } = position.coords;
-    })
-})()
+        (async function () {
+        let response = await fetch( `${API_URL}weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
+        let data = await response.json();
+        setCityWeather(data);
+        })();
+    });
+})();
 
 //-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 
@@ -21,23 +46,26 @@ const iconTemp = document.getElementById('tempIcon');
 const gettingWeather = () => {
     const inputValue = document.querySelector('.input').value;
     if (inputValue) {
-        async function getCountry() {
-            let response = await fetch(`${API_URL}${inputValue}&APPID=${API_KEY}&units=metric`);
+        setQS('#errorMessage', '');
+        (async function () {
+            let response = await fetch(`${API_URL}weather?q=${inputValue}&APPID=${API_KEY}&units=metric`);
             let info = await response.json();
-            console.log(info);
-            document.querySelector('.cityResult').innerText = inputValue;
-            document.querySelector('.countryResult').innerText = info['sys']['country'];
-            document.querySelector('.temperatureResult').innerText = `${info['main']['temp']}°C`;
-            document.querySelector('.country').style.backgroundImage = `URL(https://countryflags.io/${info['sys']['country']}/flat/64.png)`;
-        }
-        getCountry();
-
-        
-        document.getElementById('errorMessage').innerText = '';
+            setCityWeather(info);
+        })();
     } else {
-        document.getElementById('errorMessage').innerText = 'Please, enter the city name!';
+        setQS('#errorMessage', 'Please, enter the city name!');
     }
 };
+
+//-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
+//----------------input keyup ENTER-------------------------------------
+
+const enterAction = (event) => {
+    if(event.key === 'Enter') {
+        gettingWeather()
+    }
+}
 
 //-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 
