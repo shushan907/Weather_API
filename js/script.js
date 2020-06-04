10c571211cf78e09700e48e799fa8123ff572022
Shushan907 <shushan907@gmail.com>
2020-06-04 15:15:10 +0400

    add latitude and longitude

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 //------------------GetWeatherInfo-------------------------------------
 
 const API_URL = 'https://api.openweathermap.org/data/2.5/weather?q=';
-const API_KEY = 'fd48bdf8a8b87b3c140f17625f4e2d57';
+const API_KEY = '40a3ab422b6c7446253471c3714edfb8';
 const flagCountry = document.getElementById('CountryImg'); 
 const iconTemp = document.getElementById('tempIcon');
 
@@ -10,17 +10,26 @@ const iconTemp = document.getElementById('tempIcon');
 //----------------------BOM navigator-----------------------------------
 
 (function () {
-    window.navigator.geolocation.getCurrentPosition((position))
-})
+    window.navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        console.log(latitude, 'latitude');
+        console.log(longitude, 'longitude')
+    })
+})()
 
 //-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 
 //--------------------Search City---------------------------------------
 const gettingWeather = () => {
     const inputValue = document.querySelector('.input').value;
-    console.log(inputValue)
     if (inputValue) {
-        fetch(`${API_URL}${inputValue}&appid=${API_KEY}&units=metric`)
+        async function getCountry() {
+            let response = await fetch(`${API_URL}${inputValue}&appid=${API_KEY}&units=metric`);
+            let info = await response.json();
+            console.log(info)
+        }
+        getCountry()
+        
         document.getElementById('errorMessage').innerText = '';
     } else {
         document.getElementById('errorMessage').innerText = 'Please, enter the city name!';
@@ -35,4 +44,4 @@ addEventListener('click', () => {
     document.querySelector('.input').style.outline = "none";
 })
 
-//-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+//-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
